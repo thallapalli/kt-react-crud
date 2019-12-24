bb82fa2a9c0d6fb82c4707f9e98e25370e784516
karnakar <karnakar@ymail.com>
2019-12-24 01:42:14 -0500

    List notes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-//import ListNotesComponent from './components/listnotes/ListNotesComponent';
+import ListNotesComponent from './components/listnotes/ListNotesComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    Welcome to KT First ever React App!!!!
-   
+    Welcome to KT First ever React App1!!!!
+   <ListNotesComponent/>
     </div>
   );
 }
---
+++ src/components/listnotes/ListNotesComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ListNotesComponent extends Component {
+    render() {
+        return (
+              <h1>ListNotesComponent</h1>
+        )
+    }
+}
+
+export default InstructorApp
