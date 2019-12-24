07ceeec5cee27eb3d44f3ae6469b6cb83015c16e
karnakar <karnakar@ymail.com>
2019-12-24 01:08:06 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+//import ListNotesComponent from './components/listnotes/ListNotesComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Good Job KT !! Welcome to React!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Welcome to KT First ever React App!!!!
+   
     </div>
   );
 }
---
+++ src/service/CreateNoteService.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+
+const API_URL ='http://ktcrud-app-dev.ca-central-1.elasticbeanstalk.com';
+const API_URL_RETRIEVEALLNOTES = `${API_URL}/retrievenotes`
+
+class CreateNoteService {
+
+    retrieveAllCourses(name) {
+        return axios.get(API_URL_RETRIEVEALLNOTES);
+    }
+}
+
+export default new CreateNoteService()
