1d661d4d96ac704185b4cfb24ba0e7624644003a
karnakar <karnakar@ymail.com>
2019-12-23 22:32:18 -0500

    src included

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Good Job KT !! Welcome to React!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Welcome to KT First ever React App !!!!
     </div>
   );
 }
